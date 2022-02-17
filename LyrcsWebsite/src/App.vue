c272f2aa8da43d2f0531e964bfef1793ff940a5f
Patrick <patrickbdev@users.noreply.github.com>
2022-02-17 14:27:39 +0400

    WIP

---
+++ LyrcsWebsite/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Hero from './components/Hero.vue'
+import MainFeatures from './components/MainFeatures.vue'
+import FeatureList from './components/FeatureList.vue'
+import CTA from './components/CTA.vue'
+import Footer from './components/Footer.vue'
+import ComingSoonApps from './components/ComingSoonApps.vue'
+</script>
+
+<template>
+    <Hero />
+    <MainFeatures />
+    <FeatureList />
+    <ComingSoonApps />
+    <CTA />
+    <Footer />
+</template>
---
+++ LyrcsWebsite/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [    
+    "./index.html",   
+    "./src/**/*.{vue,js,ts,jsx,tsx}",  
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'lyrcs-purple': '#9253bd',
+        'lyrcs-blue': '#16c2bf'
+      }
+    },
+  },
+  plugins: [],
+}
