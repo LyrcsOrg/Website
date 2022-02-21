9dfb8de105e7dde7abf2ac339515dc10f2df9afa
Patrick <patrickbdev@users.noreply.github.com>
2022-02-21 11:11:15 +0400

    WIP

---
+++ LyrcsWebsite/src/components/CTAContactUs.vue
@@ -0,0 +1,14 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="bg-white">
+      <div class="rounded-lg lg:mx-auto lg:max-w-7xl lg:px-8 bg-gradient-to-r from-app-primary to-app-secondary">
+    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+        <span class="block">Contact Us</span>
+      </h2>
+      <p class="mt-4 text-lg leading-6 text-white">We're a friendly bunch at Lyrcs so send us a message and we'll get back to you as soon as possible.</p>
+      <a href="#" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto">Email</a>
+    </div>
+    </div>
+  </div>
+</template>
--- LyrcsWebsite/src/pages/Support.vue
+++ LyrcsWebsite/src/pages/Support.vue
@@ -2,15 +2,18 @@
 import FAQ from '@/components/FAQ.vue'
 import KeyboardShortcuts from '@/components/KeyboardShortcuts.vue'
 import PageTitle from '@/components/PageTitle.vue';
-import Footer from '@/components/Footer.vue';
+import Footer from '../components/Footer.vue';
 import SectionTitle from '../components/SectionTitle.vue';
+import CTAContactUs from '../components/CTAContactUs.vue';
 </script>
 
 <template>
     <PageTitle title="Support"></PageTitle>
     <SectionTitle title="Frequently Asked Questions" subtitle="See if any of these answer what's on your mind."></SectionTitle>
-    <FAQ></FAQ>
+    <FAQ />
     <SectionTitle title="Keyboard Shortcuts" subtitle="Try these to speed up your workflow."></SectionTitle>
-    <KeyboardShortcuts></KeyboardShortcuts>
-    <Footer></Footer>
+    <KeyboardShortcuts />
+    <SectionTitle title="Maybe we missed something?" subtitle="Let us know."></SectionTitle>
+    <CTAContactUs />
+    <Footer />
 </template>
